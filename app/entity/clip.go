b4ff212cfa1efb734c772efda8b0e825f4de272b
Tin Nguyen <nguyentrongtin2331@gmail.com>
2018-10-06 19:24:27 +0700

    Fix log printing

--- app/entity/clip.go
+++ app/entity/clip.go
@@ -143,8 +143,8 @@ func (c clipEntity) ExtractFromStream() (filename string, err error) {
 		return filename, nil
 	}
 
-	cmd := fmt.Sprintf("`./script-extract-mkv.sh %s &>> ./log/script.log`", filename)
-	_, err = exec.Command("sh", "-c", cmd).Output()
+	cmd := fmt.Sprintf("./script-extract-mkv.sh %s &>> ./log/script.log", filename)
+	err = exec.Command("bash", "-c", cmd).Run()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -154,8 +154,8 @@ func (c clipEntity) ExtractFromStream() (filename string, err error) {
 }
 
 func (c clipEntity) ExecuteCutCommand(mf, mt, sf, st int, filename, name string) error {
-	cmd := fmt.Sprintf("`./script-cut.sh %s %d %d %d %d %s &>> ./log/script.log`", filename, mf, sf, mt, st, name)
-	_, err := exec.Command("sh", "-c", cmd).Output()
+	cmd := fmt.Sprintf("./script-cut.sh %s %d %d %d %d %s &>> ./log/script.log", filename, mf, sf, mt, st, name)
+	err := exec.Command("bash", "-c", cmd).Run()
 	if err != nil {
 		fmt.Println(err)
 		return err
